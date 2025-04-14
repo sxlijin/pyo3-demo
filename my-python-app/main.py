f5df5360598c5c2ae8112d040ddc17306fc637a4
Sam Lijin <dev@sxlijin.com>
2025-04-14 11:56:40 -0700

    start the demo using a tokio server

--- my-python-app/main.py
+++ my-python-app/main.py
@@ -3,11 +3,7 @@ import my_rust_crate
 
 def main():
     print("Hello from my-python-app!")
-    # my_rust_crate.start_server_via_tokio("127.0.0.1:3000")
-    obj = my_rust_crate.MyRustObject.my_static_method("Hello from Python!")
-    obj2 = my_rust_crate.MyRustObject.my_static_method("Hello from Python!")
-    d = {obj: "Hello from Python!", obj2: "Hello from Python2!"}
-    print(d)
+    my_rust_crate.start_server_via_tokio("127.0.0.1:3000")
 
 
 if __name__ == "__main__":
